86fb946933f3921285d60ceb70dac11f74ac27a2
unknown <khaledjamal5946@gmail.com>
2024-12-29 14:21:28 +0330

    migrations .

--- movie/models.py
+++ movie/models.py
@@ -3,10 +3,11 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     genre = models.CharField(max_length=100)
+    rating = models.FloatField()
     year = models.IntegerField()
     release_date = models.DateField()
-    director = models.CharField(max_length=100)
-    actors = models.TextField()
+    director = models.CharField(max_length=100, blank=True, null=True)
+    actors = models.TextField(blank=True, null=True)
     
     
     
