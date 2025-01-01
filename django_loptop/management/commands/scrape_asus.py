893c8a5899dea16ac073441ae6df4a751e51e452
Saman-naruee <samannaruee@gmail.com>
2025-01-01 21:41:30 +0330

    Find the solution to scrape all loptops from a brand and even all brands themself!.

--- django_loptop/management/commands/scrape_asus.py
+++ django_loptop/management/commands/scrape_asus.py
@@ -1,13 +1,13 @@
-from django.core.management.base import BaseCommand
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-from car.car.spiders.asus_laptops import AsusLaptopsSpider
+# from django.core.management.base import BaseCommand
+# from scrapy.crawler import CrawlerProcess
+# from scrapy.utils.project import get_project_settings
+# from car.car.spiders.asus_laptops import AsusLaptopsSpider
 
-class Command(BaseCommand):
-    help = 'Run Asus laptops Scrapy spider'
+# class Command(BaseCommand):
+#     help = 'Run Asus laptops Scrapy spider'
 
-    def handle(self, *args, **options):
-        process = CrawlerProcess(get_project_settings())
-        process.crawl(AsusLaptopsSpider)
-        process.start()
-        self.stdout.write(self.style.SUCCESS('Successfully scraped Asus laptops data'))
+#     def handle(self, *args, **options):
+#         process = CrawlerProcess(get_project_settings())
+#         process.crawl(AsusLaptopsSpider)
+#         process.start()
+#         self.stdout.write(self.style.SUCCESS('Successfully scraped Asus laptops data'))
