3c9fe30caae994829b9ec3f91c0d8ab5d3793f96
SamanNaruee <narueesaman@gmail.com>
2025-01-01 10:30:35 +0330

    update strucured spider for cars.

--- car/car/spiders/divar_car.py
+++ car/car/spiders/divar_car.py
@@ -4,32 +4,21 @@ from django_car.models import Car
 
 class CarSpider(scrapy.Spider):
     name = "divar_car"
-    allowed_domains = ["divar.ir"]
+    allowed_domains = [
+            "digikala.com",
+            "api.digikala.com",
+            "www.digikala.com",
+            "divar.ir",
+            "api.divar.ir",
+        ]
     start_urls = [
-        # "https://divar.ir",
-        "https://divar.ir/s/tehran/vehicles",
-        # "https://api.divar.ir/v8/postlist/w/search"
+        "https://api.digikala.com/v1/categories/notebook-netbook-ultrabook/brands/asus/search/?page=1",
     ]
 
     def parse(self, response):
-        print_to_log(response.text())
-        # print_to_log('hello')
-        # for car in response.css('div.kt-post-card'):
-        #     print_to_log('Into the for')
-        #     title = car.css('h2.kt-post-card__title::text').get()
-        #     price = car.css('div.kt-post-card__description::text').get()
-        #     details = car.css('span.kt-post-card__detail::text').getall()
-        #     print_to_log(title)
-        #     car_data = {
-        #         'title': title,
-        #         'price': price,
-        #         'year': details[0] if details else '',
-        #         'source_url': response.urljoin(car.css('a::attr(href)').get()),
-        #         'extra_data': {'details': details}
-        #     }
-        #     yield car_data
-        return
-
+        yield {
+            'data' : json.loads(response.body)
+        }
 
 def print_to_log(value):
     print('########################')
