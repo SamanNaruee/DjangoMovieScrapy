321fb7a8a4840d834a08338e8bbbd62ede432ec9
SamanNaruee <narueesaman@gmail.com>
2025-01-06 14:33:30 +0330

    update admin for phone model.

--- django_loptop/admin.py
+++ django_loptop/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from django_loptop.models import Laptop
+from django_loptop.models import Laptop, Phones
 
 @admin.register(Laptop)
 class LaptopAdmin(admin.ModelAdmin):
     list_display = ('title', 'price', 'brand', 'model', 'category', 'specs', 'image_urls', 'source_url', 'created_at', 'extra_data', 'crawled_at')
+    list_filter = ('brand', 'category', 'created_at', 'crawled_at')
+    search_fields = ('title', 'brand', 'model', 'category')
+    ordering = ('-created_at',)
+
+@admin.register(Phones)
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'brand', 'model', 'category', 'image_urls', 'source_url', 'created_at', 'extra_data', 'crawled_at')
     list_filter = ('brand', 'category', 'created_at', 'crawled_at')
     search_fields = ('title', 'brand', 'model', 'category')
     ordering = ('-created_at',)
